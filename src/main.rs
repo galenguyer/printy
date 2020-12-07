280f9745e07028c60523dbce8d3c1c476f70d34f
Galen Guyer <galen@galenguyer.com>
2020-12-06 20:05:09 -0800

    add useless argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+use std::env;
 use std::io::{self, BufRead};
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        // no arguments passed
+        1 => {
+            default();
+        }
+        _ => {
+            default();
+        }
+    }
+}
+
+fn default() {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         println!("{}", line.unwrap());
